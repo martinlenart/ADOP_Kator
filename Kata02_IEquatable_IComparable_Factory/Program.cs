e4651dbcb4fce31d7a2500b875a9ef64f601d82c
Martin Lenart <martin_lenart@icloud.com>
2022-12-08 15:51:23 +0100

    update

--- Kata02_IEquatable_IComparable_Factory/Program.cs
+++ Kata02_IEquatable_IComparable_Factory/Program.cs
@@ -20,19 +20,30 @@ IMemberList RadissonMembers = MemberList.Factory.CreateRandom(20);
 RadissonMembers.Sort();
 Console.WriteLine(RadissonMembers);
 
-Console.WriteLine($"\nHilton member[0]: {HiltonMembers[0]}");
+Console.WriteLine("\nTest indexer");
+Console.WriteLine($"Hilton member[0]: {HiltonMembers[0]}");
 Console.WriteLine($"Radisson member[0]: {RadissonMembers[0]}");
 Console.WriteLine();
 
-Console.WriteLine("Test the copy constructor");
+Console.WriteLine("\nTest counter");
+Console.WriteLine($"Nr HiltonMembers joined {HiltonMembers[0].Since.Year}: " +
+    $"{HiltonMembers.Count(HiltonMembers[0].Since.Year)}");
+Console.WriteLine($"Nr RadissonMembers joined {RadissonMembers[0].Since.Year}: " +
+    $"{HiltonMembers.Count(RadissonMembers[0].Since.Year)}");
+
+Console.WriteLine("\nTest the deep copy using copy constructor");
 
 //I make a cast IMemberList -> MemberList of HiltonMembers to be able to use
 //the copy contructor as the list _member is private
 IMemberList copyList = new MemberList((MemberList)HiltonMembers);
-Console.WriteLine(copyList);
+
+Console.WriteLine("Before change:");
+Console.WriteLine(HiltonMembers[10]);
 Console.WriteLine(copyList[10]);
 
 HiltonMembers[10].FirstName = "Test";
+Console.WriteLine("After change:");
+Console.WriteLine(HiltonMembers[10]);
 Console.WriteLine(copyList[10]);
 
 
